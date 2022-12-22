76b334a22a59e81bf7776c72dd39773ff6a2057f
enesarikk <enesarik2093@gmail.com>
2022-12-23 00:32:24 +0300

    KDV ödevi

--- KDVHesaplama/src/Main.java
+++ KDVHesaplama/src/Main.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        double price, KDVoran = 0.18;
+        double price, KDVrate=0.18, KDVprice, KDVwprice;
         Scanner inp = new Scanner(System.in);
         System.out.print("Ürünün fiyatını giriniz : ");
         price = inp.nextDouble();
-        System.out.println("KDV'li fiyat : " + String.format("%.2f", (price * KDVoran + price)));
+        KDVprice = price * KDVrate;
+        KDVwprice = price + KDVprice;
+        System.out.println("KDV'siz fiyatı : " + price);
+        System.out.println("KDV oranı : " + KDVrate);
+        System.out.println("KDV fiyatı : " + KDVprice);
+        System.out.println("KDV'li fiyatı : " + KDVwprice);
 
     }
 }
