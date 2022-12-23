3290e0cf53db16270ffa7dc5cd4b206b3b7718e8
enesarikk <enesarik2093@gmail.com>
2022-12-23 10:48:50 +0300

    Duzenlendi

--- KDVHesaplama/src/Main.java
+++ KDVHesaplama/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         double price, KDVrate=0.18, KDVprice, KDVwprice;
         Scanner inp = new Scanner(System.in);
-        System.out.print("Ürünün fiyatını giriniz : ");
+        System.out.print("Ürünün fiyatını giriniz :");
         price = inp.nextDouble();
         KDVprice = price * KDVrate;
         KDVwprice = price + KDVprice;
