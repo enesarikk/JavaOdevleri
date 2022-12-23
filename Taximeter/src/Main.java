fb66ee693ce4704b40dc9eef3b91d7f4b315d8bf
enesarikk <enesarik2093@gmail.com>
2022-12-23 15:42:22 +0300

    New Projcet

---
+++ Taximeter/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int km;
+        double perKM = 2.20, total, startPrice = 10;
+
+        Scanner road = new Scanner(System.in);
+        System.out.print("Mesafeyi KM cinsinden giriniz. : ");
+        km = road.nextInt();
+        total = (km * perKM) + startPrice;
+        total = (total < 20) ? 20 : total;
+        System.out.print("Toplam tutar : " + total);
+    }
+}
