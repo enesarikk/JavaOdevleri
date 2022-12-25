e3553db2f84412e6b9271090846194cd092c937d
enesarikk <enesarik2093@gmail.com>
2022-12-25 20:29:21 +0300

    PassingClass duzenlendi.

--- PassingClass/src/Main.java
+++ PassingClass/src/Main.java
@@ -16,7 +16,25 @@ public class Main {
         System.out.print("Müzik notunu giriniz. : ");
         muzik = put.nextInt();
 
-        double avarage = (mat + fizik + kimya + turkce + muzik) / 5;
+        double avarage, total = 0;
+
+        if(0 <= mat && mat <=100){
+            total = total + mat;
+        }
+        if(0 <= fizik && fizik <=100){
+            total = total + fizik;
+        }
+        if(0 <= kimya && kimya <=100){
+            total = total + kimya;
+        }
+        if (0 <= turkce && turkce <=100){
+            total = total + turkce;
+        }
+        if (0 <= muzik && muzik <=100){
+            total = total + muzik;
+        }
+
+        avarage = total / 5;
 
         if(avarage <= 55){
             System.out.println("Sınıfta kaldınız. Seneye tekrar deneyiniz :) ");
