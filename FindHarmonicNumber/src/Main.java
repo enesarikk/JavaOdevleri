ebc256ab12dea72f571685740ebaf5f3496a1dc4
enesarikk <enesarik2093@gmail.com>
2023-01-02 16:58:10 +0300

    Harmonic

---
+++ FindHarmonicNumber/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = scan.nextInt();
+        double result = 0.0;
+        for (double i = 1; i <= n; i++) {
+            result += (1/i);
+        }
+        System.out.println("The result of your harmonic number : " + result);
+    }
+}
